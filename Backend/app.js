b0ea8950902c174da6c089130fb3e700984b345e
PIYUSH956 <piyushjaiswal956@gmail.com>
2024-12-08 16:04:40 +0530

    Removed MongoURL and Added in env

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 
-mongoose.connect('mongodb+srv://abpj150:ffGZfBKegIVxWBxx@cluster0.te0io.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected'))
   .catch(error => console.error('Database connection error:', error));
 
